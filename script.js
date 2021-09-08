0ba2f44c4d65a9697506445023ef0cf7e431740a
lazarnarcis <lazarnarcis203@yahoo.com>
2021-09-09 00:20:54 +0300

    update

--- script.js
+++ script.js
@@ -68,19 +68,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
     document.documentElement.setAttribute("data-theme", "light");
     var themeSwitcher = document.getElementById("theme-switcher");
     themeSwitcher.onclick = function() {
-		themeSwitchers.style = "transform: scale(0.1)";
+		themeSwitcher.style = "transform: scale(0)";
+		var currentTheme = document.documentElement.getAttribute("data-theme");
+		var switchToTheme = currentTheme === "dark" ? "light" : "dark";
+		document.documentElement.setAttribute("data-theme", switchToTheme);
+		setTimeout(function() {
+			themeSwitcher.style = "transform: scale(1)";
+		}, 300);
+    }
+	var themeSwitchers = document.getElementById("theme-switchers");
+    themeSwitchers.onclick = function() {
+		themeSwitchers.style = "transform: scale(0)";
 		var currentTheme = document.documentElement.getAttribute("data-theme");
 		var switchToTheme = currentTheme === "dark" ? "light" : "dark";
 		document.documentElement.setAttribute("data-theme", switchToTheme);
 		setTimeout(function() {
 			themeSwitchers.style = "transform: scale(1)";
 		}, 300);
-    }
-	var themeSwitchers = document.getElementById("theme-switchers");
-    themeSwitchers.onclick = function() {
-		themeSwitchers.style = "transform: scale(0.1)";
-		var currentTheme = document.documentElement.getAttribute("data-theme");
-		var switchToTheme = currentTheme === "dark" ? "light" : "dark";
-		document.documentElement.setAttribute("data-theme", switchToTheme);
     }
 });
