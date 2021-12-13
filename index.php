a04ca992cccdeb00b701617829a8d5bb04afef0d
Lazar Narcis <lnarcis310@gmail.com>
2021-12-13 21:15:52 +0200

    UPdate

--- index.php
+++ index.php
@@ -22,9 +22,9 @@
 		$mail->IsSMTP(); // enable SMTP
 		$mail->SMTPDebug = 0; // debugging: 1 = errors and messages, 2 = messages only
 		$mail->SMTPAuth = true; // authentication enabled
-		$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+		$mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for Gmail
 		$mail->Host = "smtp.gmail.com";
-		$mail->Port = 465; // or 587
+		$mail->Port = 587; // or 587
 		$mail->IsHTML(true);
 		$mail->Username = "$email";
 		$mail->Password = "$password";
