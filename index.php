5b1bdf87f00162b004cdec5b538ace84964a0a9c
Lazar Narcis <lnarcis310@gmail.com>
2021-12-14 16:13:27 +0200

    Each email sent will be saved in the SQL database.

--- index.php
+++ index.php
@@ -6,7 +6,22 @@
 
 	$email = getenv('GMAIL_EMAIL');
 	$password = getenv('GMAIL_PASSWORD');
-	
+
+	$whitelist = array(
+		'127.0.0.1',
+		'::1'
+	);
+	if(!in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
+		$username_db = getenv("username_db");
+		$password_db = getenv("password_db");
+		$host_db = getenv("host_db");
+		$database_db = getenv("database_db");
+	} else {
+		$username_db = "root";
+		$password_db = "";
+		$database_db = "emails";
+		$host_db = "localhost";
+	}
 	require 'PHPMailer-master/src/Exception.php';
 	require 'PHPMailer-master/src/PHPMailer.php';
 	require 'PHPMailer-master/src/SMTP.php';
@@ -37,6 +52,12 @@
 			$err = "Mailer Error: " . $mail->ErrorInfo;
 		} 
 		if ($mail->Send()) {
+			$link = mysqli_connect($host_db, $username_db, $password_db, $database_db);
+			if ($link === false) {
+				die("ERROR! Could not connect to database!");
+			}
+			$sql = "INSERT INTO emails (email, subject, message, name) VALUES ('$emailtoSend', '$subject', '$message', '$firstname $lastname')";
+			mysqli_query($link, $sql);
 			$err = "Message has been sent!";
 			header('Location: index.php');
 			exit();
---
+++ portfolio.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 14, 2021 at 03:11 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `portfolio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emails`
+--
+
+CREATE TABLE `emails` (
+  `id` int(11) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `subject` text NOT NULL,
+  `message` text NOT NULL,
+  `name` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
