8618105010bbb1d6a1a38c8c36826564a6f82db0
Narcis Lazar <lnarcis310@gmail.com>
2022-03-06 19:42:30 +0200

    set AI for user id

--- portfolio.sql
+++ portfolio.sql
@@ -1,31 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Dec 14, 2021 at 03:11 PM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `portfolio`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `emails`
---
 
 CREATE TABLE `emails` (
   `id` int(11) NOT NULL,
@@ -35,8 +10,11 @@ CREATE TABLE `emails` (
   `name` text NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ALTER TABLE `emails`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `emails`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+COMMIT;
