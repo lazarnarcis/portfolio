50dab9a018a6a67767b1def62c4d6f03d4120685
Narcis Lazar <lnarcis310@gmail.com>
2022-04-22 13:11:57 +0300

    new updates

--- script.js
+++ script.js
@@ -16,46 +16,7 @@ typeText();
 let footer = document.getElementById('footer-content');
 let presentYear = new Date().getFullYear();
 footer.innerHTML = "&copy; Narcis " + presentYear;
-// jquery
-$(document).ready(function() {   
-	let xa = 0;
-	xa = $(window).scrollTop();
-	if (xa > 150) {
-		$('#top').css('display', 'inline');
-		$('#desktop-nav').css('background-color', 'var(--nav-background)');
-		$('.nav-bar-phone').css('background-color', 'var(--nav-background)');
-		$('.nav-bar').css('box-shadow', 'var(--nav-shadow)');
-		$('.nav-bar-phone').css('box-shadow', 'var(--nav-shadow)');
-	} 
-	let scroll_pos = 0;
-	$(document).scroll(function() { 
-		scroll_pos = $(this).scrollTop();
-		if (scroll_pos > 150) {
-			$('#desktop-nav').css('background-color', 'var(--nav-background)');
-			$('.nav-bar-phone').css('background-color', 'var(--nav-background)');
-			$('.nav-bar').css('box-shadow', 'var(--nav-shadow)');
-			$('.nav-bar-phone').css('box-shadow', 'var(--nav-shadow)');
-			$('#top').css('display', 'inline');
-		} else if (scroll_pos < 150) {
-			$('#top').css('display', 'none');
-			$('#desktop-nav').css('background', 'none');
-			$('.nav-bar').css('box-shadow', '0px 0px 0px grey');
-			$('.nav-bar-phone').css('box-shadow', '0px 0px 0px grey');
-			$('.nav-bar-phone').css('background', 'none');
-		}
-	});
-	$(window).scroll(function(){
-        if ($(this).scrollTop() > 150) {
-            $('.scrollToTop').fadeIn();
-        } else {
-            $('.scrollToTop').fadeOut();
-        }
-    });
-    $('.scrollToTop').click(function(){
-        $('html, body').animate({scrollTop : 0},800);
-        return false;
-    });
-});
+
 let modal = document.getElementById("modal");
 let modalContent = document.getElementsByClassName("modal-content")[0];
 let btn = document.getElementById("menulogo");
