9c1d91f3dd19703841a43bf70eedcec361f026cc
Narcis Lazar <lnarcis310@gmail.com>
2022-04-22 14:00:20 +0300

    Update config.php

--- config.php
+++ config.php
@@ -7,17 +7,18 @@
     if(!in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
         $username_db = getenv('username_db');
         $password_db = getenv('password_db');
-        $database_db = getenv('$database_db');
-        $host_db = getenv('$host_db');
+        $database_db = getenv('database_db');
+        $host_db = getenv('host_db');
+        $email = getenv('GMAIL_EMAIL');
+        $password = getenv('GMAIL_PASSWORD');
     } else {
         $username_db = "root";
         $password_db = "";
         $database_db = "portfolio";
         $host_db = "localhost";
+        $email = "example@contact.com";
+        $password = "myPassword123";
     }
-
-    $email = getenv('GMAIL_EMAIL');
-    $password = getenv('GMAIL_PASSWORD');
 
     $link = mysqli_connect($host_db, $username_db, $password_db, $database_db);
     if (!$link) {
