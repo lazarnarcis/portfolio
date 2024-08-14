a2bd7f16cf8971c3ef84e0b77547489cf69c3603
root <root@vps-bda90107.vps.ovh.net>
2024-08-14 12:42:29 +0000

    add composer vendor folder

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit3e740b41696969a22244159237a4e958
+class ComposerStaticInit108be68e4e2b97fed51d36a10eed0849
 {
     public static $files = array (
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
@@ -72,9 +72,9 @@ class ComposerStaticInit3e740b41696969a22244159237a4e958
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit3e740b41696969a22244159237a4e958::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit3e740b41696969a22244159237a4e958::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInit3e740b41696969a22244159237a4e958::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit108be68e4e2b97fed51d36a10eed0849::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit108be68e4e2b97fed51d36a10eed0849::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit108be68e4e2b97fed51d36a10eed0849::$classMap;
 
         }, null, ClassLoader::class);
     }
